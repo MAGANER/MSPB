b2ffad5e8c3f078f5e6f2920b2385c511591dacf
MAGANER <maganer.krik@mail.ru>
2022-10-22 17:27:32 +0300

    need to rewrite creating/opening

--- CommandProcessor.cpp
+++ CommandProcessor.cpp
@@ -3,15 +3,30 @@
 CommandProcessor::CommandProcessor()
 {
 	commands["q"] = [&](const vector<string>&) { exit(-1); };
+	commands["cls"] = [&](const vector<string>&) {system("cls"); };
 	commands["new"] = [&](const vector<string>& args)
 	{
 		if (check_arg_number(args, 2))
 			create_new_box(args[0], args[1]);
 	};
+	commands["open"] = [&](const vector<string>& args)
+	{
+		if (check_arg_number(args, 2))
+			open_box(args[0], args[1]);
+	};
+	commands["md"] = [&](const vector<string>& args)
+	{
+		if (!args.empty())
+			make_directory(args[0],args);
+	};
+	commands["ls"] = [&](const vector<string>& args)
+	{
+		show_all_directories();
+	};
 }
 CommandProcessor::~CommandProcessor()
 {
-	if (db != nullptr) delete db;
+	if (box != nullptr)delete box;
 }
 void CommandProcessor::process_command(const pair <string, vector<string>>& command)
 {
@@ -30,11 +45,10 @@ void CommandProcessor::error(ErrorType type)
 }
 void CommandProcessor::create_new_box(const string& dir, const string& key)
 {
-	db = new DataBase(dir, key, true);
-	if (db->is_ok())
-		cout << dir << " created sucessfully!" << endl;
-	else
-		cout << dir << " error:" << db->get_error_message() << endl;
+}
+void CommandProcessor::open_box(const string& dir, const string& key)
+{
+
 }
 bool CommandProcessor::check_arg_number(const vector<string>& args, int right_number)
 {
@@ -44,4 +58,20 @@ bool CommandProcessor::check_arg_number(const vector<string>& args, int right_nu
 		return false;
 	}
 	return true;
+}
+string CommandProcessor::get_cur_box()const
+{
+	return curr_box;
+}
+void CommandProcessor::make_directory(const string& dir_name, const vector<string>& files)
+{
+
+}
+bool CommandProcessor::check_table_exists(const string& name)
+{
+
+}
+void CommandProcessor::show_all_directories()
+{
+
 }
--- CommandProcessor.h
+++ CommandProcessor.h
@@ -5,27 +5,37 @@
 #include<string>
 #include<vector>
 #include<iostream>
-#include"sql/DataBase.h"
+#include<filesystem>
+#include<fstream>
+#include"json.hpp"
 using namespace std;
-using namespace SQLite3DataBaseTools;
+namespace fs = std::filesystem;
+using json = nlohmann::json;
+
 class CommandProcessor
 {
 private:
 	map<string, function<void(const vector<string>&)>> commands;
-	DataBase* db = nullptr;
-
+	string curr_box;
+	json* box = nullptr;
 	enum class ErrorType
 	{
 		NotDefinedCommand
 	};
 	void error(ErrorType type);
 	void create_new_box(const string& dir, const string& key);
+	void open_box(const string& dir, const string& key);
+	void make_directory(const string& dir_name, const vector<string>& files);
+	void show_all_directories();
+
+	bool check_table_exists(const string& name);
 	bool check_arg_number(const vector<string>& args, int right_number);
 public:
 	CommandProcessor();
 	~CommandProcessor();
 
 	void process_command(const pair < string, vector<string>>& command);
+	string get_cur_box()const;
 };
 #endif
 
